17a964e79b595dbf8d8fb5c50ca213638643b8d1
maCucyrt07 <46973482+maCucyrt07@users.noreply.github.com>
2019-02-04 14:45:57 -0500

    Updated from Brython Server: 2/4/2019 2:45:56 PM

--- calculatepi.py
+++ calculatepi.py
@@ -1,7 +1,7 @@
 """
 calculatepi.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: MaCucyrt07
+Credit: none
 Assignment:
 
 Write and submit a Python program that computes an approximate value of π by calculating the following sum:
